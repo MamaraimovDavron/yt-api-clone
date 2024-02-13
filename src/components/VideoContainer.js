61e8bf5348039489fea4987fe9c06255aa3787b6
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2024-02-13 11:18:40 +0500

    API has been added

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { YOUTUBE_VIDEO_API, API_KEY } from "../constant/youtube";
 
 export const VideoContainer = () => {
-  return <div>VideoContainer</div>;
+  const fetchingYoutubeVideo = async () => {
+    try {
+      const res = await axios.get(`${YOUTUBE_VIDEO_API}`);
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchingYoutubeVideo();
+  }, []);
+  return <div></div>;
 };
